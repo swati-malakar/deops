049c595bc2889b3c267898acdfd30d154541a30d
DELL <DELL@DESKTOP-9LVK55H>
2024-08-22 14:34:02 +0530

    commit

---
+++ deops/src/generic/generic_photo.java
@@ -0,0 +1,16 @@
+package generic;
+
+import java.util.Date;
+
+public class generic_photo {
+   
+	public static void getPhoto()
+	{
+		String photo="./photos/";
+		Date d=new Date();
+		String d1=d.toString();
+		String d2 = d1.replaceAll(":", "-");
+		Takesscreent
+	}
+
+}
